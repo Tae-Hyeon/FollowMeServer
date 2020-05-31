64ffb05fb47f39010a840d84994593c0c483a9d6
Tae-Hyeon <hanth7028@gmail.com>
2020-05-31 20:02:02 +0900

    Server DB, sequelize 기본 설정
    DB config, sequelize index

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var passport = require('passport');
+//const passportConfig = require('./passport');
+var { sequelize } = require('sequelize');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+require('dotenv').config();
+
+// sync
+models.sequelize.sync().then( () => {
+  console.log(" DB 연결 성공");
+}).catch(err => {
+  console.log("연결 실패");
+  console.log(err);
+});
+//passportConfig(passport);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
